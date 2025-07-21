babee54f7353a9c98359e24453f48a36061e2904
Nugie kurniawan <nugiebinhar@gmail.com>
2025-07-21 22:53:31 +0700

    Membuat linkedlist sederhana sendiri

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -1,26 +1,32 @@
 #include<iostream>
 using namespace std;
 
-	struct Node{
-		int angka;
-		Node* next;
-	};
+struct Node{
+	int data;
+	Node* next;
+};
 
 void print(Node* head){
 	Node* current = head;
 	while(current != NULL){
-		cout<<current->angka<<"->";
+		cout<<current->data<<"->";
 		current = current->next;
 	}
 	cout<<"NULL"<<endl;
 }
 int main(){
-
-	Node* one = new Node();
-		Node* two = new Node();
-	one->angka = 99;
-	one->next = two;
-	two->angka = 30;
-	two->next = NULL;
-	print(one);
+	Node* head = new Node();
+	Node* second = new Node();
+	Node* three = new Node();
+	
+	head->data = 99;
+	head->next = second;
+	
+	second->data = 30;
+	second->next = three;
+	
+	three->data = 27;
+	three->next = NULL;
+	
+	print(head);
 }
